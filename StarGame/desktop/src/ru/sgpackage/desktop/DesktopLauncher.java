4ad495c6561cbd082f322ae40dca9d96fb318931
gg <gartanolio@yandex.ru>
2019-01-13 17:58:09 +0300

    ДЗ1 Android_1_5 задача минимум

---
+++ StarGame/desktop/src/ru/sgpackage/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ru.sgpackage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.sgpackage.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("user.name", "Public");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
