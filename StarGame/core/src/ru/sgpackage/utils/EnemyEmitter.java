df4cacf8a00194077e8b38fab7a3d9b8527a73f3
gg <gartanolio@yandex.ru>
2019-02-11 19:09:31 +0300

    1_5_ ДЗ8 v 1.4 Изменение баланса с повышением уровня сложности (на 10 лвле трудно выжить, стало интереснее играть)

--- StarGame/core/src/ru/sgpackage/utils/EnemyEmitter.java
+++ StarGame/core/src/ru/sgpackage/utils/EnemyEmitter.java
@@ -12,27 +12,27 @@ import ru.sgpackage.sprite.game.Enemy;
 public class EnemyEmitter {
 
     private static final float ENEMY_SMALL_HEIGHT = 0.1f;    //размер маленького корабля
-    private static final float ENEMY_SMALL_BULLET_HEIGHT = 0.01f;    //размер пули маленького корабля
-    private static final float ENEMY_SMALL_BULLET_VY = -0.5f;    //скорость и направление маленькой пули
+    private static final float ENEMY_SMALL_BULLET_HEIGHT = 0.015f;    //размер пули маленького корабля
+    private static final float ENEMY_SMALL_BULLET_VY = -0.6f;    //скорость и направление маленькой пули
     private static final int ENEMY_SMALL_DAMAGE = 1;    //урон маленькой пули
-    private static final float ENEMY_SMALL_RELOAD_INTERVAL = 2f;    //перезарядка малой пули
+    private static final float ENEMY_SMALL_RELOAD_INTERVAL = 1.5f;    //перезарядка малой пули
     private static final int ENEMY_SMALL_HP = 1;    //здоровье малого противника
 
     private static final float ENEMY_MIDDLE_HEIGHT = 0.15f;
     private static final float ENEMY_MIDDLE_BULLET_HEIGHT = 0.02f;    //размер пули среднего корабля
-    private static final float ENEMY_MIDDLE_BULLET_VY = -0.4f;    //скорость и направление средней пули
+    private static final float ENEMY_MIDDLE_BULLET_VY = -0.5f;    //скорость и направление средней пули
     private static final int ENEMY_MIDDLE_DAMAGE = 3;
-    private static final float ENEMY_MIDDLE_RELOAD_INTERVAL = 2.5f;   //перезарядка средней пули
+    private static final float ENEMY_MIDDLE_RELOAD_INTERVAL = 2f;   //перезарядка средней пули
     private static final int ENEMY_MIDDLE_HP = 3;
 
     private static final float ENEMY_BIG_HEIGHT = 0.2f;
     private static final float ENEMY_BIG_BULLET_HEIGHT = 0.03f;    //размер пули большого корабля
-    private static final float ENEMY_BIG_BULLET_VY = -0.3f;    //скорость и направление большой пули
+    private static final float ENEMY_BIG_BULLET_VY = -0.4f;    //скорость и направление большой пули
     private static final int ENEMY_BIG_DAMAGE = 5;
-    private static final float ENEMY_BIG_RELOAD_INTERVAL = 3f;      //перезарядка большой пули
+    private static final float ENEMY_BIG_RELOAD_INTERVAL = 2.5f;      //перезарядка большой пули
     private static final int ENEMY_BIG_HP = 5;
 
-    private Vector2 enemySmallV = new Vector2(0, -0.2f);        //вектор скорости движения малого корабля
+    private Vector2 enemySmallV = new Vector2(0, -0.25f);        //вектор скорости движения малого корабля
     private Vector2 enemyMiddleV = new Vector2(0, -0.175f);        //вектор скорости движения среднего корабля
     private Vector2 enemyBigV = new Vector2(0, -0.15f);        //вектор скорости движения большого корабля
     private TextureRegion[] enemySmallRegion;
@@ -68,6 +68,7 @@ public class EnemyEmitter {
     //метод генерирования кораблей
     public void generate(float delta, int frags) {
         level = frags / 10 + 1;
+        generateInterval = 3f - level/5f;               //изменение скорости генерации противников с лвлом
         generateTimer += delta;
         if(generateTimer >= generateInterval) {
             generateTimer = 0f;
@@ -79,9 +80,9 @@ public class EnemyEmitter {
                         enemySmallV,
                         bulletRegion,
                         ENEMY_SMALL_BULLET_HEIGHT,
-                        ENEMY_SMALL_BULLET_VY,
+                        ENEMY_SMALL_BULLET_VY - level/100f,
                         ENEMY_SMALL_DAMAGE * level,         // урон увеличивается с каждым уровнем
-                        ENEMY_SMALL_RELOAD_INTERVAL,
+                        ENEMY_SMALL_RELOAD_INTERVAL- level/100f,
                         ENEMY_SMALL_HEIGHT,
                         ENEMY_SMALL_HP,
                         worldBounds
@@ -92,9 +93,9 @@ public class EnemyEmitter {
                         enemyMiddleV,
                         bulletRegion,
                         ENEMY_MIDDLE_BULLET_HEIGHT,
-                        ENEMY_MIDDLE_BULLET_VY,
+                        ENEMY_MIDDLE_BULLET_VY - level/100f,
                         ENEMY_MIDDLE_DAMAGE * level,
-                        ENEMY_MIDDLE_RELOAD_INTERVAL,
+                        ENEMY_MIDDLE_RELOAD_INTERVAL - level/100f,
                         ENEMY_MIDDLE_HEIGHT,
                         ENEMY_MIDDLE_HP,
                         worldBounds
@@ -105,9 +106,9 @@ public class EnemyEmitter {
                         enemyBigV,
                         bulletRegion,
                         ENEMY_BIG_BULLET_HEIGHT,
-                        ENEMY_BIG_BULLET_VY,
+                        ENEMY_BIG_BULLET_VY - level/100f,
                         ENEMY_BIG_DAMAGE * level,
-                        ENEMY_BIG_RELOAD_INTERVAL,
+                        ENEMY_BIG_RELOAD_INTERVAL - level/100f,
                         ENEMY_BIG_HEIGHT,
                         ENEMY_BIG_HP,
                         worldBounds
